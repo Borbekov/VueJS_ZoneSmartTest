f51ed0688ef9ae0830728041e679218ec72bde53
Bekzhan Borbekov <b.borbekov@kassa24.kz>
2021-02-24 21:41:16 +0600

    project init

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    {{hello}}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data: () => ({
+    hello: "hello"
+  })
+}
+</script>
+
+<style lang="scss">
+
+</style>
